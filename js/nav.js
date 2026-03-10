d0defdc92ee13d175e26b1df5f229212311de928
Ali Aktas <alintras@proton.me>
2026-03-10 22:35:05 +0100

    fix(nav): update links to use absolute paths for fun page items

--- js/nav.js
+++ js/nav.js
@@ -27,9 +27,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
     <details>
       <summary>🎮 Fun</summary>
-      ${item("🎲", "pages/fun/simple-physics.html", "Simple Physics")}
-      ${item("🏝️", "pages/fun/castaway/castaway.html", "Castaway")}
-      ${item("❗", "pages/fun/mroops.html", "Mr.Oops!!")}
+      ${item("🎲", "/pages/fun/simple-physics.html", "Simple Physics")}
+      ${item("🏝️", "/pages/fun/castaway/castaway.html", "Castaway")}
+      ${item("❗", "/pages/fun/mroops.html", "Mr.Oops!!")}
       ${item("💘", "/pages/fun/love-calculator.html", "Love Compatibility Calculator")}
       ${item("👩‍❤️‍💋‍👨", "/pages/fun/vivi.html", "Valentines 2026")}
       ${item("⭕", "/pages/fun/perfect-circle.html", "Perfect Circle Game")}
