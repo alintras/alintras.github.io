449598153d6f4880c478255c5f320de661349593
Ali Aktas <alintras@proton.me>
2026-03-13 23:22:49 +0100

    add multiple smiley GIFs and PNGs to the assets folder

--- js/nav.js
+++ js/nav.js
@@ -10,50 +10,50 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   sidebar.innerHTML = `
-    <b id="nav-toggle">Navigation</b>
+    <b id="nav-toggle">Navigation </b>
     <div id="sidebar-body"><br><br>
     🏠&#xFE0E; <a href="/index.html">Home</a><br><br>
     <details>
-      <summary>🛠️ Tools</summary>
-      ${item("✉️", "/pages/tools/wondermail/wondermail.html", "PMD RT Wondermail Generator")}
-      ${item("💣", "/pages/tools/voltorb-solver.html", "HGSS Voltorb Solver")}
-      ${item("🔢", "/pages/tools/cps-counter.html", "CPS Counter")}
-      ${item("🔒", "/pages/tools/password-generator.html", "Password Generator")}
-      ${item("📐", "/pages/tools/unit-converter.html", "Unit Converter")}
-      ${item("💬", "/pages/tools/random-quote-generator.html", "Quote Generator")}
+      <summary><img src="/assets/smileys/pickaxe.png" style="width:16px; height:16px; vertical-align:middle; image-rendering:pixelated;"> Tools</summary>
+      ${item(`<img src="/assets/smileys/mail.png" style="width:16px;height:16px;vertical-align:middle;image-rendering:pixelated;">`, "/pages/tools/wondermail/wondermail.html", "PMD RT Wondermail Generator")}
+      ${item(`<img src="/assets/smileys/bomb.png" style="width:16px;height:16px;vertical-align:middle;image-rendering:pixelated;">`, "/pages/tools/voltorb-solver.html", "HGSS Voltorb Solver")}
+      ${item(`<img src="/assets/smileys/count.png" style="width:16px;height:16px;vertical-align:middle;image-rendering:pixelated;">`, "/pages/tools/cps-counter.html", "CPS Counter")}
+      ${item(`<img src="/assets/smileys/lock.png" style="width:16px;height:16px;vertical-align:middle;image-rendering:pixelated;">`, "/pages/tools/password-generator.html", "Password Generator")}
+      ${item(`<img src="/assets/smileys/unit-calc.png" style="width:16px;height:16px;vertical-align:middle;image-rendering:pixelated;">`, "/pages/tools/unit-converter.html", "Unit Converter")}
+      ${item(`<img src="/assets/smileys/thbubble.png" style="width:16px;height:16px;vertical-align:middle;image-rendering:pixelated;">`, "/pages/tools/random-quote-generator.html", "Quote Generator")}
     </details><br>
     <details>
       <summary><img src="/assets/smileys/laugh.gif" style="width:16px; height:16px; vertical-align:middle; image-rendering:pixelated;"> Fun</summary>
-      ${item("🎲", "/pages/fun/simple-physics.html", "Simple Physics")}
-      ${item("💘", "/pages/fun/love-calculator.html", "Love Compatibility Calculator")}
-      ${item("👩‍❤️‍💋‍👨", "/pages/fun/vivi.html", "Valentines 2026")}
-      ${item("🪪", "/pages/fun/ausweis.html", "ID Generator")}
-      ${item("🚗", "/pages/fun/fuehrerschein.html", "License Generator")}
-      ${item("🪙", "/pages/fun/muenze.html", "Coin Generator")}
-      ${item("📰", "/pages/fun/nachrichten.html", "News Generator")}
-      ${item("🎴", "/pages/fun/sammelkarte.html", "Trading Card Generator")}
-      ${item("📜", "/pages/fun/urkunde.html", "Certificate Generator")}
-      ${item("🗞️", "/pages/fun/zeitung.html", "Newspaper Generator")}
-      ${item("📇", "/pages/fun/zeugnis.html", "Report Card Generator")}
-      ${item("🧩", "/pages/fun/puzzle.html", "Custom Puzzle Generator")}
+      ${item(`<img src="/assets/smileys/dice.png" style="width:16px;height:16px;vertical-align:middle;image-rendering:pixelated;">`, "/pages/fun/simple-physics.html", "Simple Physics")}
+      ${item(`<img src="/assets/smileys/love-calc.png" style="width:16px;height:16px;vertical-align:middle;image-rendering:pixelated;">`, "/pages/fun/love-calculator.html", "Love Compatibility Calculator")}
+      ${item(`<img src="/assets/smileys/valentine.png" style="width:16px;height:16px;vertical-align:middle;image-rendering:pixelated;">`, "/pages/fun/vivi.html", "Valentines 2026")}
+      ${item(`<img src="/assets/smileys/ID.png" style="width:16px;height:16px;vertical-align:middle;image-rendering:pixelated;">`, "/pages/fun/ausweis.html", "ID Generator")}
+      ${item(`<img src="/assets/smileys/car.png" style="width:16px;height:16px;vertical-align:middle;image-rendering:pixelated;">`, "/pages/fun/fuehrerschein.html", "License Generator")}
+      ${item(`<img src="/assets/smileys/coin.png" style="width:16px;height:16px;vertical-align:middle;image-rendering:pixelated;">`, "/pages/fun/muenze.html", "Coin Generator")}
+      ${item(`<img src="/assets/smileys/news.png" style="width:16px;height:16px;vertical-align:middle;image-rendering:pixelated;">`, "/pages/fun/nachrichten.html", "News Generator")}
+      ${item(`<img src="/assets/smileys/cards.png" style="width:16px;height:16px;vertical-align:middle;image-rendering:pixelated;">`, "/pages/fun/sammelkarte.html", "Trading Card Generator")}
+      ${item(`<img src="/assets/smileys/cert.png" style="width:16px;height:16px;vertical-align:middle;image-rendering:pixelated;">`, "/pages/fun/urkunde.html", "Certificate Generator")}
+      ${item(`<img src="/assets/smileys/newsp.png" style="width:16px;height:16px;vertical-align:middle;image-rendering:pixelated;">`, "/pages/fun/zeitung.html", "Newspaper Generator")}
+      ${item(`<img src="/assets/smileys/note.gif" style="width:16px;height:16px;vertical-align:middle;image-rendering:pixelated;">`, "/pages/fun/zeugnis.html", "Report Card Generator")}
+      ${item(`<img src="/assets/smileys/puzzle.png" style="width:16px;height:16px;vertical-align:middle;image-rendering:pixelated;">`, "/pages/fun/puzzle.html", "Custom Puzzle Generator")}
     </details><br>
     <details>
       <summary><img src="/assets/smileys/mario.gif" style="width:16px; height:16px; vertical-align:middle; image-rendering:pixelated;"> Games</summary>
-      ${item("🏝️", "/pages/games/castaway/castaway.html", "Castaway")}
-      ${item("❗", "/pages/games/mroops.html", "Mr.Oops!!")}
-      ${item("❌", "/pages/games/infinite-ttt.html", "Infinite TicTacToe")}
-      ${item("🏓", "/pages/games/pong.html", "Pong")}
-      ${item("⭕", "/pages/games/perfect-circle.html", "Perfect Circle Game")}
+      ${item(`<img src="/assets/smileys/isle.gif" style="width:16px;height:16px;vertical-align:middle;image-rendering:pixelated;">`, "/pages/games/castaway/castaway.html", "Castaway")}
+      ${item(`<img src="/assets/smileys/excl.png" style="width:16px;height:16px;vertical-align:middle;image-rendering:pixelated;">`, "/pages/games/mroops.html", "Mr.Oops!!")}
+      ${item(`<img src="/assets/smileys/X.png" style="width:16px;height:16px;vertical-align:middle;image-rendering:pixelated;">`, "/pages/games/infinite-ttt.html", "Infinite TicTacToe")}
+      ${item(`<img src="/assets/smileys/joystick.png" style="width:16px;height:16px;vertical-align:middle;image-rendering:pixelated;">`, "/pages/games/pong.html", "Pong")}
+      ${item(`<img src="/assets/smileys/O.png" style="width:16px;height:16px;vertical-align:middle;image-rendering:pixelated;">`, "/pages/games/perfect-circle.html", "Perfect Circle Game")}
     </details><br>
     <details>
-      <summary>📚 Stuff</summary>
-      ${item("💡", "/pages/stuff/git.html", "Git")}
-      ${item("👩‍💻", "/pages/stuff/html.html", "HTML")}
-      ${item("👨‍💻", "/pages/stuff/c.html", "C")}
-      ${item("🧮", "/pages/stuff/math-history.html", "Math History")}
-      ${item("🧮", "/pages/stuff/math-prereq.html", "Math Prerequisites")}
+      <summary><img src="/assets/smileys/notebook.png" style="width:16px; height:16px; vertical-align:middle; image-rendering:pixelated;"> Stuff</summary>
+      ${item(`<img src="/assets/smileys/bulb.png" style="width:16px;height:16px;vertical-align:middle;image-rendering:pixelated;">`, "/pages/stuff/git.html", "Git")}
+      ${item(`<img src="/assets/smileys/code-woman.png" style="width:16px;height:16px;vertical-align:middle;image-rendering:pixelated;">`, "/pages/stuff/html.html", "HTML")}
+      ${item(`<img src="/assets/smileys/code-man.png" style="width:16px;height:16px;vertical-align:middle;image-rendering:pixelated;">`, "/pages/stuff/c.html", "C")}
+      ${item(`<img src="/assets/smileys/counter.png" style="width:16px;height:16px;vertical-align:middle;image-rendering:pixelated;">`, "/pages/stuff/math-history.html", "Math History")}
+      ${item(`<img src="/assets/smileys/counter.png" style="width:16px;height:16px;vertical-align:middle;image-rendering:pixelated;">`, "/pages/stuff/math-prereq.html", "Math Prerequisites")}
     </details><br>
-    ${item("🫂", "/pages/about.html", "About")}
+    ${item(`<img src="/assets/smileys/person.png" style="width:16px;height:16px;vertical-align:middle;image-rendering:pixelated;">`, "/pages/about.html", "About")}
     </div>
   `;
 
